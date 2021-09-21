3a1145ecb23980addb867300f77cad682364f0a6
Chisty Md.Muzammel Hossain <chistyz10@gmail.com>
2021-09-21 20:03:58 +0600

    fetchingChanges

--- resources/views/user/userDashboard.blade.php
+++ resources/views/user/userDashboard.blade.php
@@ -1,15 +1,21 @@
-<div class="row">
-    <div class="col-4">
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title text-center">Heavy Work</h5>
+
+            <div class="w-50 m-auto" style="width: 50rem;">
+           
+                <h5 class="card-title text-center">Volunteer Work Type</h5>
                 <form action="{{route('search')}}" method="get">
-                    <label for="forWork">Options</label>
+                    <label for="forWork">Select WorkType</label>
                     <select class="form-select" name="worktype" id="">
                         <option value="heavyA">HeavyA</option>
                         <option value="heavyB">HeavyB</option>
                         <option value="heavyC">HeavyC</option>
+                        <option value="moderateA">ModerateA</option>
+                        <option value="moderateB">ModerateB</option>
+                        <option value="moderateC">ModerateC</option>
+                        <option value="liteA">LiteA</option>
+                        <option value="liteB">LiteB</option>
+                        <option value="liteC">LiteC</option>
                     </select>
+           
                     <label for="forWork">Area</label>
                     <select class="form-select" name="area" id="">
                        <option value="dhaka">Dhaka</option>
@@ -21,26 +27,4 @@
                         <button type="submit" class="btn btn-primary">Search Volunteer</button>
                     </div>
                 </form>
-            </div>
-        </div>
-
-    </div>
-    <div class="col-4">
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title text-center">Card title</h5>
-
-            </div>
-        </div>
-
-    </div>
-    <div class="col-4">
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title text-center">Card title</h5>
-
-            </div>
-        </div>
-
-    </div>
-</div>
+             </div>
